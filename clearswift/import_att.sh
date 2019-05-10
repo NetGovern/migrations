8e4e9c1e0b193630aa89b7df16be97a8db05053e
ClearSwift <jeremie.salaun@clearswift.com>
2019-05-10 12:02:43 -0400

    import_att.sh edited online with Bitbucket
    Edited following the problems we faced here

--- clearswift/import_att.sh
+++ clearswift/import_att.sh
@@ -1,18 +1,39 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+# Argument: zip file with at least the templates, filenames, and rules directory at the top level.
+# Usage: ./import_pmm.sh file.zip
+
+# Check if running as root
+if [ $EUID -ne 0 ]; then
+        echo "[!] This script must be run as root"
+        echo "[-] Exiting the script."
+        exit 1
+fi
 
 echo "Processing Media type templates" 
+# Define variables from the argument
 EXPORTED_ZIP_FILE="$1"
-mkdir templates
-cp $EXPORTED_ZIP_FILE templates/
-cd templates
+UI_RESTART=0
+
 unzip $EXPORTED_ZIP_FILE
-cp *.xml /var/cs-gateway/uicfg/policy/rules/
+# Test if the templates directory is present in the current directory. Otherwise ignore and move on.
+if [ -d templates ]; then
+	cp templates/*.xml /var/cs-gateway/uicfg/policy/rules/
+    UI_RESTART=1
+    else echo "No templates directory. Ignoring"
+fi
 
 echo "Processing Attachment blocking rules and file lists"
-cd ../
-cp filenames/*.xml /var/cs-gateway/uicfg/policy/filenames/
-cp rules/*.xml /var/cs-gateway/uicfg/policy/rules/
+if [[ -d filenames && -d rules ]]; then
+	cp filenames/*.xml /var/cs-gateway/uicfg/policy/filenames/
+	cp rules/*.xml /var/cs-gateway/uicfg/policy/rules/
+    UI_RESTART=1
+    else echo "No filenames or rules directory. Ignoring"
+fi
 
-cs-servicecontrol restart tomcat
-
+if [ UI_RESTART -eq 1]; then
+	echo "Changes made. Restart tomcat"
+	cs-servicecontrol restart tomcat
+fi
+    
 echo "Please login to Clearswift UI to apply the configuration"
